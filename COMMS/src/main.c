77dff0dbffed4b277524afc8d1c1d3b8fd01992f
deNeedo <jkudrys@protonmail.com>
2025-11-07 09:52:55 +0100

    Added basic structure for building project and running tests

---
+++ COMMS/src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "utils.h"
+
+int main() {
+    printf("Starting communication system test...\n");
+    test_utils();
+    printf("All systems operational!\n");
+    return 0;
+}
---
+++ COMMS/tests/sample.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <assert.h>
+
+int main() {
+    printf("Running sample test...\n");
+    assert(1 + 1 == 2);
+    printf("Sample test passed.\n");
+    return 0;
+}
